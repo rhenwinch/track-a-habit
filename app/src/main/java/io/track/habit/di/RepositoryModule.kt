c2c851fb4d54482c8711aa745cdfb2b1aa8ff59b
rhenwinch <rhenwinchester@gmail.com>
2025-05-27 15:19:29 +0800

    feat(di): add repository and asset reader dependencies to Hilt module
    Updates the `RepositoryModule` to provide dependencies for `StreakRepository` and `AssetReader`.
    
    - Adds `provideAndroidAssetReader` to create a singleton instance of `AndroidAssetReader`, which implements `AssetReader`. This uses `@ApplicationContext` to get the application context.
    - Adds `provideStreakRepository` to create a singleton instance of `StreakRepositoryImpl`, which implements `StreakRepository`. This takes an `AssetReader` as a dependency.

--- app/src/main/java/io/track/habit/di/RepositoryModule.kt
+++ app/src/main/java/io/track/habit/di/RepositoryModule.kt
@@ -1,12 +1,18 @@
 package io.track.habit.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import io.track.habit.data.local.assets.AndroidAssetReader
 import io.track.habit.data.local.database.dao.HabitDao
 import io.track.habit.data.repository.HabitRepositoryImpl
+import io.track.habit.data.repository.StreakRepositoryImpl
+import io.track.habit.domain.repository.AssetReader
 import io.track.habit.domain.repository.HabitRepository
+import io.track.habit.domain.repository.StreakRepository
 import javax.inject.Singleton
 
 @Module
@@ -16,5 +22,19 @@ object RepositoryModule {
     @Provides
     fun provideHabitRepository(habitDao: HabitDao): HabitRepository {
         return HabitRepositoryImpl(habitDao)
+    }
+
+    @Singleton
+    @Provides
+    fun provideAndroidAssetReader(
+        @ApplicationContext context: Context,
+    ): AssetReader {
+        return AndroidAssetReader(context)
+    }
+
+    @Singleton
+    @Provides
+    fun provideStreakRepository(assetReader: AssetReader): StreakRepository {
+        return StreakRepositoryImpl(assetReader)
     }
 }
