41c89be75abc349aae06795faad936968fb14d34
rhenwinch <rhenwinchester@gmail.com>
2025-06-03 22:26:27 +0800

    refactor(usecase): improve `GetRandomQuoteUseCase`
    Refactors the `GetRandomQuoteUseCase` to:
    - Initialize the `quotes` list in an `init` block using a coroutine launched on the IO dispatcher. This moves the potentially blocking file reading operation out of the `invoke` function and handles it asynchronously during object creation.
    - Modify the `invoke` operator function to be non-suspending. If `quotes` is not yet initialized (which could happen if the `init` block's coroutine hasn't completed), it now uses `runBlocking` to synchronously read the quotes. This ensures a quote is always returned, though it might block if the initial asynchronous loading is slow.

--- app/src/main/java/io/track/habit/domain/usecase/GetRandomQuoteUseCase.kt
+++ app/src/main/java/io/track/habit/domain/usecase/GetRandomQuoteUseCase.kt
@@ -3,6 +3,8 @@ package io.track.habit.domain.usecase
 import com.google.gson.Gson
 import io.track.habit.domain.model.Quote
 import io.track.habit.domain.repository.AssetReader
+import io.track.habit.domain.utils.coroutines.AppDispatcher.Companion.launchOnIO
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -17,10 +19,17 @@ class GetRandomQuoteUseCase
         private val gson: Gson by lazy { Gson() }
         private lateinit var quotes: List<Quote>
 
-        suspend operator fun invoke(): Quote {
+        init {
+            launchOnIO {
+                val quotesAsset = assetReader.read(QUOTES_FILE_NAME)
+                quotes = gson.fromJson(quotesAsset, Array<Quote>::class.java).toList()
+            }
+        }
+
+        operator fun invoke(): Quote {
             if (::quotes.isInitialized) return quotes.random()
 
-            val quotesAsset = assetReader.read(QUOTES_FILE_NAME)
+            val quotesAsset = runBlocking { assetReader.read(QUOTES_FILE_NAME) }
             quotes = gson.fromJson(quotesAsset, Array<Quote>::class.java).toList()
 
             return quotes.random()
