59f7688908003dc6f518d209613c8502da50aca9
rhenwinch <rhenwinchester@gmail.com>
2025-06-07 18:50:22 +0800

    test(viewmodel): enhance `HabitsViewModelTest` with reset progress and habit showcase tests
    - Added tests for `resetProgress` method to verify habit updates and log creation.
    - Implemented tests for `onHabitLongClick` to ensure correct state updates for showcased habits.
    - Updated existing tests to improve coverage and reliability.

--- app/src/test/java/io/track/habit/viewmodel/HabitsViewModelTest.kt
+++ app/src/test/java/io/track/habit/viewmodel/HabitsViewModelTest.kt
@@ -2,21 +2,25 @@ package io.track.habit.viewmodel
 
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import app.cash.turbine.test
-import app.cash.turbine.turbineScope
 import io.mockk.coEvery
 import io.mockk.mockk
 import io.track.habit.data.local.database.entities.Habit
 import io.track.habit.datastore.FakeSettingsDataStore
+import io.track.habit.domain.model.HabitWithStreak
+import io.track.habit.domain.model.Streak
 import io.track.habit.domain.repository.AssetReader
+import io.track.habit.domain.repository.HabitLogsRepository
 import io.track.habit.domain.repository.HabitRepository
 import io.track.habit.domain.usecase.GetHabitsWithStreaksUseCase
 import io.track.habit.domain.usecase.GetRandomQuoteUseCase
 import io.track.habit.domain.usecase.GetStreaksByDaysUseCase
 import io.track.habit.domain.usecase.QUOTES_FILE_NAME
 import io.track.habit.domain.utils.SortOrder
+import io.track.habit.repository.fake.FakeHabitLogsRepository
 import io.track.habit.repository.fake.FakeHabitRepository
 import io.track.habit.repository.fake.FakeStreakRepository
 import io.track.habit.ui.screens.habits.HabitsViewModel
+import io.track.habit.ui.screens.habits.composables.ResetDetails
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
@@ -25,26 +29,17 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotNull
+import strikt.assertions.isSameInstanceAs
 import java.util.Date
 
-/**
- * Unit tests for the [HabitsViewModel].
- * This class tests the various functionalities of the [HabitsViewModel], including:
- * - Initialization and UI state updates.
- * - Habit retrieval, addition, updating, and deletion.
- * - Selection and deselection of habits.
- * - Sorting and filtering of habits.
- * - Toggling of UI dialogs and flags.
- * - Censorship of habit names.
- *
- * It utilizes fake repositories and a test dispatcher to ensure isolated and controlled testing environments.
- *
- * For flow testing, we use turbine. We also add delay of 250ms for each collection since its shit
- */
 @OptIn(ExperimentalCoroutinesApi::class)
 class HabitsViewModelTest {
     private lateinit var assetReader: AssetReader
     private lateinit var habitRepository: HabitRepository
+    private lateinit var habitLogsRepository: HabitLogsRepository
     private lateinit var viewModel: HabitsViewModel
 
     private val testDispatcher = StandardTestDispatcher()
@@ -71,6 +66,8 @@ class HabitsViewModelTest {
                 """.trimIndent()
 
             habitRepository = FakeHabitRepository()
+            habitLogsRepository = FakeHabitLogsRepository()
+
             val getRandomQuoteUseCase = GetRandomQuoteUseCase(assetReader, testDispatcher)
             val streakRepository = FakeStreakRepository()
 
@@ -95,6 +92,7 @@ class HabitsViewModelTest {
                     getRandomQuoteUseCase = getRandomQuoteUseCase,
                     habitRepository = habitRepository,
                     settingsDataStore = testSettingsDataStore,
+                    habitLogsRepository = habitLogsRepository,
                     ioDispatcher = testDispatcher,
                 )
 
@@ -102,315 +100,129 @@ class HabitsViewModelTest {
         }
 
     @Test
-    fun `getUiState reflects quote update after init`() =
-        assert(
-            viewModel.uiState.value.quote.message == "Test Quote",
-        )
+    fun `resetProgress should update habit and create log`() =
+        runTest(testDispatcher) {
+            val habitId = 1L
+            val habit = Habit(habitId = habitId, name = "Test Habit", lastResetAt = Date(1000))
+            val resetDetails = ResetDetails(habitId = habitId, trigger = "Test Trigger", notes = "Test Notes")
 
-    @Test
-    fun `getHabits returns initial empty list`() {
-        assert(viewModel.habits.value.isEmpty())
-    }
+            habitRepository.insertHabit(habit)
 
-    @Test
-    fun `getHabits emits habits from use case`() =
-        runTest {
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
+            viewModel.resetProgress(resetDetails)
+            advanceUntilIdle()
+
+            val updatedHabit = habitRepository.getHabitById(habitId)
+            expectThat(updatedHabit).isNotNull()
+
+            habitLogsRepository.getHabitLogsByHabitId(habitId).test {
+                val logs = awaitItem()
+
+                expectThat(logs.size).isEqualTo(1)
+                expectThat(logs).isNotNull()
+                expectThat(logs[0]) {
+                    get { habitId }.isEqualTo(habitId)
+                    get { trigger }.isEqualTo("Test Trigger")
+                    get { notes }.isEqualTo("Test Notes")
+                }
             }
+        }
+
+    @Test
+    fun `onHabitLongClick should update longPressedHabit in state`() =
+        runTest {
+            val streak =
+                Streak(
+                    title = "Test Streak",
+                    minDaysRequired = 0,
+                    maxDaysRequired = 10,
+                    badgeIcon = "icon",
+                    message = "Test message",
+                )
+
+            val habitWithStreak =
+                HabitWithStreak(
+                    habit = Habit(habitId = 1, name = "Test Habit"),
+                    streak = streak,
+                )
+
+            viewModel.onHabitLongClick(habitWithStreak)
+
+            viewModel.uiState.test {
+                val state = awaitItem()
+                expectThat(state.longPressedHabit).isSameInstanceAs(habitWithStreak)
+            }
+        }
+
+    @Test
+    fun `onSortOrderSelect should update sortOrder in state`() =
+        runTest {
+            val newSortOrder = SortOrder.Creation()
+
+            viewModel.onSortOrderSelect(newSortOrder)
+
+            viewModel.uiState.test {
+                val state = awaitItem()
+                expectThat(state.sortOrder).isEqualTo(newSortOrder)
+            }
+        }
+
+    @Test
+    fun `toggleCensorshipOnNames should update isCensoringHabitNames in state`() =
+        runTest {
+            val censorship = true
+
+            viewModel.toggleCensorshipOnNames(censorship)
+
+            viewModel.uiState.test {
+                val state = awaitItem()
+                expectThat(state.isCensoringHabitNames).isEqualTo(censorship)
+            }
+        }
+
+    @Test
+    fun `toggleShowcaseHabit should update habitToShowcase in state and update settings`() =
+        runTest(testDispatcher) {
+            val streak =
+                Streak(
+                    title = "Test Streak",
+                    minDaysRequired = 0,
+                    maxDaysRequired = 10,
+                    badgeIcon = "icon",
+                    message = "Test message",
+                )
+
+            val habitWithStreak =
+                HabitWithStreak(
+                    habit = Habit(habitId = 1, name = "Test Habit"),
+                    streak = streak,
+                )
+
+            viewModel.toggleShowcaseHabit(habitWithStreak)
+            advanceUntilIdle()
+
+            viewModel.uiState.test {
+                val state = awaitItem()
+                expectThat(state.habitToShowcase).isSameInstanceAs(habitWithStreak)
+                cancelAndConsumeRemainingEvents()
+            }
+        }
+
+    @Test
+    fun `habits flow should emit habits from repository`() =
+        runTest {
+            val habit1 = Habit(habitId = 1, name = "Test Habit 1")
+            val habit2 = Habit(habitId = 2, name = "Test Habit 2")
+
+            habitRepository.insertHabit(habit1)
+            habitRepository.insertHabit(habit2)
 
             viewModel.habits.test {
                 skipItems(1)
-                val habits = awaitItem()
-                assert(habits.isNotEmpty())
 
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `deleteHabit calls repository delete`() =
-        runTest {
-            val habit = Habit(habitId = 1, name = "Test habit")
-            habitRepository.insertHabit(habit)
-
-            val insertedHabit = habitRepository.getHabitById(habit.habitId)
-            assert(insertedHabit != null)
-
-            viewModel.deleteHabit(habit)
-
-            val deletedHabit = habitRepository.getHabitById(habit.habitId)
-            assert(deletedHabit == null)
-        }
-
-    @Test
-    fun `delete selected habits and unselects it all`() =
-        runTest {
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt =
-                                Date().apply {
-                                    time -= ((i + 3) * 24 * 60 * 60 * 1000)
-                                },
-                        ),
-                )
-            }
-
-            turbineScope {
-                val habitsTurbine = viewModel.habits.testIn(backgroundScope)
-                val uiStateTurbine = viewModel.uiState.testIn(backgroundScope)
-
-                with(habitsTurbine) {
-                    skipItems(1)
-                    awaitItem().forEach {
-                        viewModel.toggleSelectionOnHabit(it)
-                    }
-                }
-
-                with(uiStateTurbine) {
-                    val selectedHabits = expectMostRecentItem().selectedHabits
-                    assert(selectedHabits.size == 5)
-
-                    selectedHabits.forEach {
-                        habitRepository.deleteHabit(it.habit)
-                    }
-
-                    viewModel.unselectAll()
-
-                    val updatedSelectedHabits = awaitItem().selectedHabits
-                    assert(updatedSelectedHabits.isEmpty())
-
-                    cancelAndConsumeRemainingEvents()
-                }
-
-                advanceUntilIdle()
-                assert(habitsTurbine.expectMostRecentItem().isEmpty())
-                habitsTurbine.cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `toggleSelectionOnHabit adds unselected habit`() =
-        runTest {
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
-            }
-
-            turbineScope {
-                val habitsTurbine = viewModel.habits.testIn(backgroundScope)
-                val uiStateTurbine = viewModel.uiState.testIn(backgroundScope)
-
-                with(habitsTurbine) {
-                    skipItems(1)
-                    val habits = awaitItem()
-                    viewModel.toggleSelectionOnHabit(habits[0])
-                    cancelAndConsumeRemainingEvents()
-                }
-
-                with(uiStateTurbine) {
-                    skipItems(1)
-                    val selectedHabits = expectMostRecentItem().selectedHabits
-                    assert(selectedHabits.first().habit.name == "Habit 1")
-                    cancelAndConsumeRemainingEvents()
-                }
-            }
-        }
-
-    @Test
-    fun `toggleSelectionOnHabit removes selected habit`() =
-        runTest {
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
-            }
-
-            turbineScope {
-                val habitsTurbine = viewModel.habits.testIn(backgroundScope)
-                val uiStateTurbine = viewModel.uiState.testIn(backgroundScope)
-
-                with(habitsTurbine) {
-                    skipItems(1)
-                    val habits = awaitItem()
-                    viewModel.toggleSelectionOnHabit(habits[0])
-                    cancelAndConsumeRemainingEvents()
-                }
-
-                with(uiStateTurbine) {
-                    skipItems(1)
-                    val selectedHabits = awaitItem().selectedHabits
-                    assert(selectedHabits.first().habit.name == "Habit 1")
-
-                    viewModel.toggleSelectionOnHabit(selectedHabits.first())
-
-                    val updatedSelectedHabits = awaitItem().selectedHabits
-                    assert(updatedSelectedHabits.isEmpty())
-                    cancelAndConsumeRemainingEvents()
-                }
-            }
-        }
-
-    @Test
-    fun `unselectAll clears selected habits`() =
-        runTest {
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
-            }
-
-            turbineScope {
-                val habitsTurbine = viewModel.habits.testIn(backgroundScope)
-                val uiStateTurbine = viewModel.uiState.testIn(backgroundScope)
-
-                with(habitsTurbine) {
-                    skipItems(1)
-                    val habits = awaitItem()
-                    habits.forEach {
-                        viewModel.toggleSelectionOnHabit(it)
-                    }
-
-                    cancelAndConsumeRemainingEvents()
-                }
-
-                with(uiStateTurbine) {
-                    val selectedHabits = expectMostRecentItem().selectedHabits
-                    assert(selectedHabits.size == 5)
-
-                    viewModel.unselectAll()
-
-                    val updatedSelectedHabits = awaitItem().selectedHabits
-                    assert(updatedSelectedHabits.isEmpty())
-
-                    cancelAndConsumeRemainingEvents()
-                }
-            }
-        }
-
-    @Test
-    fun `toggleSortOrder updates sort order in UiState`() =
-        runTest {
-            viewModel.toggleSortOrder(SortOrder.Name())
-            viewModel.uiState.test {
-                val updatedSortOrder = expectMostRecentItem().sortOrder
-                assert(updatedSortOrder is SortOrder.Name)
-
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `toggleDeleteConfirmation updates flag in UiState`() =
-        runTest {
-            viewModel.toggleDeleteConfirmation(true)
-            viewModel.uiState.test {
-                val updatedFlag = expectMostRecentItem().isShowingDeleteConfirmation
-                assert(updatedFlag)
-
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `toggleAddDialog updates flag in UiState`() =
-        runTest {
-            viewModel.toggleAddDialog(true)
-            viewModel.uiState.test {
-                val updatedFlag = expectMostRecentItem().isShowingAddDialog
-                assert(updatedFlag)
-
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `toggleEditDialog updates flag in UiState`() =
-        runTest {
-            viewModel.toggleEditDialog(true)
-            viewModel.uiState.test {
-                val updatedFlag = expectMostRecentItem().isShowingEditDialog
-                assert(updatedFlag)
-
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `toggleCensorshipOnNames updates flag in UiState`() =
-        runTest {
-            viewModel.toggleCensorshipOnNames(true)
-            viewModel.uiState.test {
-                val updatedFlag = expectMostRecentItem().isCensoringHabitNames
-                assert(updatedFlag)
-
-                cancelAndConsumeRemainingEvents()
-            }
-        }
-
-    @Test
-    fun `getHabits have censors applied when requested with censoring enabled`() =
-        runTest {
-            viewModel.toggleCensorshipOnNames(true)
-
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
-            }
-
-            viewModel.habits.test {
-                val habits = expectMostRecentItem()
-
-                assert(habits.all { it.habit.name.contains("*") })
-            }
-        }
-
-    @Test
-    fun `getHabits have censors applied when requested with censoring disabled`() =
-        runTest {
-            viewModel.toggleCensorshipOnNames(false)
-
-            for (i in 1..5) {
-                habitRepository.insertHabit(
-                    habit =
-                        Habit(
-                            name = "Habit $i",
-                            lastResetAt = Date().apply { time -= ((i + 3) * 24 * 60 * 60 * 1000) },
-                        ),
-                )
-            }
-
-            viewModel.habits.test {
-                val habits = expectMostRecentItem()
-
-                assert(habits.all { !it.habit.name.contains("*") })
+                val result = awaitItem()
+                expectThat(result.size).isEqualTo(2)
+                expectThat(result[0].habit.habitId).isEqualTo(1L)
+                expectThat(result[1].habit.habitId).isEqualTo(2L)
             }
         }
 }
